9c2ca55551f03acad8de96b19be65a45d25cbe40
Sonu <axs.sh7@gmail.com>
2026-04-13 18:44:01 +0530

    fixed: db configuration

--- server/db/config/db.config.js
+++ server/db/config/db.config.js
@@ -1,38 +1,27 @@
 import mongoose from "mongoose";
-// import dotenv from "dotenv";
-// dotenv.config();
 
-const dbConfig = {
-  HOST: process.env.DB_HOST || "localhost",
-  PORT: process.env.DB_PORT || 27017,
-  DB: process.env.DB_NAME   || "vems_db",
-  CLOUD_DB: process.env.DB_HOST_PROD,
+let isConnected = false;
+
+const connectDB = async () => {
+  if (isConnected) return;
+
+  try {
+    const MONGO_URI = process.env.DB_HOST_PROD; // only use cloud
+
+    if (!MONGO_URI) {
+      throw new Error("MONGO_URI not defined");
+    }
+
+    const conn = await mongoose.connect(MONGO_URI, {
+      bufferCommands: false,
+    });
+
+    isConnected = conn.connections[0].readyState;
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB Error:", error);
+    throw error;
+  }
 };
 
-// Decide URI
-const isProd = process.env.SERVER_ENV === "production";
-console.log("Environment:", process.env.SERVER_ENV);
-console.log(`Connecting to ${isProd ? "Cloud" : "Local"} MongoDB... (${isProd ? dbConfig.CLOUD_DB : `${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`})`);
-const MONGO_URI = isProd? dbConfig.CLOUD_DB : `mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`;
-
-// Connect
-mongoose.connect(MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-  .catch(err => console.error("MongoDB connection error:", err));
-const db = mongoose.connection;
-
-// Error
-db.on("error", (err) => {
-  console.error("MongoDB connection error:", err);
-});
-
-// Success
-db.once("open", () => {
-  if (isProd) {
-    console.log("☁️ MongoDB Connected (Cloud Database)");
-  } else {
-    console.log("💻 MongoDB Connected (Local Database)");
-  }
-});
-
-export default db;
+export default connectDB;
